fd6f455e9af2f9912b4e6ab9cd2f9a0ba16cc86c
lostmode <47912949+lostmode@users.noreply.github.com>
2021-08-09 21:29:20 +0300

    fixing csrf
    fixing csrf

--- Routes/apiroute.js
+++ Routes/apiroute.js
@@ -11,16 +11,16 @@ router.post('/getuseranswerinquiz', checkJwt.validateauthorization, controller.g
 router.post('/getquestioninquiz', checkJwt.validateauthorization, controller.getquestioninquiz);
 router.post('/getnextquestion', checkJwt.validateauthorization, controller.getnextquestion);
 router.post('/getprevquestion', checkJwt.validateauthorization, controller.getprevquestion);
-router.post('/admin/getuserdata', checkJwt.validateauthorization, checkJwt.validateadmin, controller.getuserdata)
-router.post('/admin/saveuserdata', checkJwt.validateauthorization, checkJwt.validateadmin, controller.saveuserdata)
-router.post('/admin/deleteuserdata', checkJwt.validateauthorization, checkJwt.validateadmin, controller.deleteuserdata)
-router.post('/admin/getquestiondata', checkJwt.validateauthorization, checkJwt.validateadmin, controller.getquestiondata)
-router.post('/admin/savequestiondata', checkJwt.validateauthorization, checkJwt.validateadmin, controller.savequestiondata)
-router.post('/admin/deletequestiondata', checkJwt.validateauthorization, checkJwt.validateadmin, controller.deletequestiondata)
-router.post('/admin/getquizdata', checkJwt.validateauthorization, checkJwt.validateadmin, controller.getquizdata)
-router.post('/admin/savequizdata', checkJwt.validateauthorization, checkJwt.validateadmin, controller.savequizdata)
-router.post('/admin/deletequizdata', checkJwt.validateauthorization, checkJwt.validateadmin, controller.deletequizdata)
-router.post('/admin/getexamdata', checkJwt.validateauthorization, checkJwt.validateadmin, controller.getexamdata)
-router.post('/admin/saveexamdata', checkJwt.validateauthorization, checkJwt.validateadmin, controller.saveexamdata)
-router.post('/admin/deleteexamdata', checkJwt.validateauthorization, checkJwt.validateadmin, controller.deleteexamdata)
+router.post('/admin/getuserdata', checkJwt.validateapiauthorization, checkJwt.validateadmin, controller.getuserdata)
+router.post('/admin/saveuserdata', checkJwt.validateapiauthorization, checkJwt.validateadmin, controller.saveuserdata)
+router.post('/admin/deleteuserdata', checkJwt.validateapiauthorization, checkJwt.validateadmin, controller.deleteuserdata)
+router.post('/admin/getquestiondata', checkJwt.validateapiauthorization, checkJwt.validateadmin, controller.getquestiondata)
+router.post('/admin/savequestiondata', checkJwt.validateapiauthorization, checkJwt.validateadmin, controller.savequestiondata)
+router.post('/admin/deletequestiondata', checkJwt.validateapiauthorization, checkJwt.validateadmin, controller.deletequestiondata)
+router.post('/admin/getquizdata', checkJwt.validateapiauthorization, checkJwt.validateadmin, controller.getquizdata)
+router.post('/admin/savequizdata', checkJwt.validateapiauthorization, checkJwt.validateadmin, controller.savequizdata)
+router.post('/admin/deletequizdata', checkJwt.validateapiauthorization, checkJwt.validateadmin, controller.deletequizdata)
+router.post('/admin/getexamdata', checkJwt.validateapiauthorization, checkJwt.validateadmin, controller.getexamdata)
+router.post('/admin/saveexamdata', checkJwt.validateapiauthorization, checkJwt.validateadmin, controller.saveexamdata)
+router.post('/admin/deleteexamdata', checkJwt.validateapiauthorization, checkJwt.validateadmin, controller.deleteexamdata)
 module.exports = router;
